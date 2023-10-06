1d23588ab6687ac65cc39b3dc1a5e8d92150ec5d
ekiidx <info@vuedesign.co>
2023-10-06 02:23:23 -0400

    updated footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,92 @@
-<footer>
+<!-- Footer -->
+<footer class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="footer-logo-item">
+                        <div class="f-logo">
+                            <a href="#"><img style="width: 170px;" src="/wp-content/themes/vue/portfolio/strongway/img/logo.svg" alt="Strong Way logo"></a>
+                        </div>
+                        <p>STRONG WAY is a group oriented classroom and gym program that can help everyday women make healthy lifestyle choices.</p>
+                        <div class="social-links">
+                            <h6>Follow us</h6>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1">
+                    <div class="footer-widget">
+                        <h5>Our Blog</h5>
+                        <div class="footer-blog">
+                            <a href="#" class="fb-item">
+                                <h6>A list of ideas for the beginner crossfit</h6>
+                                <span class="blog-time"><i class="fa fa-clock-o"></i> Jan 20, 2021</span>
+                            </a>
+                            <a href="#" class="fb-item">
+                                <h6>How to safely lift weights at the gym or at home</h6>
+                                <span class="blog-time"><i class="fa fa-clock-o"></i> Mar 15, 2021</span>
+                            </a>
+                            <a href="#" class="fb-item">
+                                <h6>How to make time for your daily workout routine</h6>
+                                <span class="blog-time"><i class="fa fa-clock-o"></i> Jul 11, 2021</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="footer-widget">
+                        <h5>Classes</h5>
+                        <ul class="workout-program">
+                            <li><a href="#">Woman's Crossfit</a></li>
+                            <li><a href="#">Weightlifting</a></li>
+                            <li><a href="#">Hot Yoga</a></li>
+                            <li><a href="#">Nutrition 101</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="footer-widget">
+                        <h5>Contact Us</h5>
+                        <ul class="footer-info">
+                            <li>
+                                <i class="fa fa-phone"></i>
+                                <span>Phone:</span>
+                                (313) 551 6789
+                            </li>
+                            <li>
+                                <i class="fa fa-envelope-o"></i>
+                                <span>Email:</span>
+                                info@strongway.com
+                            </li>
+                            <li>
+                                <i class="fa fa-map-marker"></i>
+                                <span>Address:</span>
+                                100 W Adams Ave, Kirkwood, MO 63122
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-text">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <div class="ct-inside">
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Design by <a href="https://vuedesign.co">Vue Design</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-</footer>
+    <!-- Javascript -->
+    <script src="/wp-content/themes/vue/portfolio/strongway/js/jquery-3.3.1.min.js"></script>
+    <script src="/wp-content/themes/vue/portfolio/strongway/js/bootstrap.min.js"></script>
+    <script src="/wp-content/themes/vue/portfolio/strongway/js/main.js"></script>
 
 </body>
 </html>
