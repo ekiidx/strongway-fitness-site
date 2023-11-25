20587cb3cf2f9e079cc00197a04e43a23382bc3d
ekiidx <info@vuedesign.co>
2023-11-24 22:02:56 -0500

    fixed header.php formatting

--- header.php
+++ header.php
@@ -1,65 +1,66 @@
 <!doctype html>
 <html lang="en">
-	<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<title>Strongway | Women's Fitness | Vue Design</title>
-
-	<!-- Favicon -->
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/site.webmanifest">
-	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3f1189">
-	<meta name="msapplication-TileColor" content="#000000">
-	<meta name="theme-color" content="#ffffff">
-
-	<!-- Search Engine -->
-	<meta name="description" content="Strong Way workout business website template designed by Vue Design">
-	<meta name="keywords" content="Vue Design, Design, Web Design, Graphic Design, Cleveland Web Design, Web Development">
-	<meta name="image" content="">
-	<!-- Schema.org for Google -->
-	<meta itemprop="name" content="">
-	<meta itemprop="description" content="">
-	<meta itemprop="image" content="website-preview.jpg">
-	<!-- Open Graph general (Facebook, Pinterest & Google+) -->
-	<meta name="og:title" content="">
-	<meta name="og:description" content="">
-	<!--<meta name="og:image" content="">-->
-	<meta name="og:url" content="">
-	<meta name="og:site_name" content="">
-	<meta name="og:locale" content="en_US">
-	<meta name="og:type" content="website">
-	<meta property="og:image:width" content="1200">
-	<meta property="og:image:height" content="630">
-
-	<!-- Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800,900&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
-
-	<!-- Stylesheets -->
-	<?php $phpVersion = '?v=1.0.0'; ?>
-	<link rel="stylesheet" href="/assets/css/bootstrap.min.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/font-awesome.min.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/elegant-icons.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/nice-select.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/owl.carousel.min.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/magnific-popup.css" type="text/css">
-	<link rel="stylesheet" href="/assets/css/slicknav.min.css" type="text/css">
-	<link rel="stylesheet" href="<?php echo '/assets/css/main.css' . $phpVersion; ?>">
-
-	<!--
-	_   __           ___          _
-	| | / /_ _____   / _ \___ ___ (_)__ ____
-	| |/ / // / -_) / // / -_|_-</ / _ `/ _ \
-	|___/\_,_/\__/ /____/\__/___/_/\_, /_//_/
-								/___/
-	-->
-
-	</head>
 	
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="profile" href="https://gmpg.org/xfn/11">
+<title>Strongway | Women's Fitness | Vue Design</title>
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#3f1189">
+<meta name="msapplication-TileColor" content="#000000">
+<meta name="theme-color" content="#ffffff">
+
+<!-- Search Engine -->
+<meta name="description" content="Strong Way workout business website template designed by Vue Design">
+<meta name="keywords" content="Vue Design, Design, Web Design, Graphic Design, Cleveland Web Design, Web Development">
+<meta name="image" content="">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="">
+<meta itemprop="description" content="">
+<meta itemprop="image" content="website-preview.jpg">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="">
+<meta name="og:description" content="">
+<!--<meta name="og:image" content="">-->
+<meta name="og:url" content="">
+<meta name="og:site_name" content="">
+<meta name="og:locale" content="en_US">
+<meta name="og:type" content="website">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,700,800,900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+
+<!-- Stylesheets -->
+<?php $phpVersion = '?v=1.0.0'; ?>
+<link rel="stylesheet" href="/assets/css/bootstrap.min.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/elegant-icons.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/nice-select.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/owl.carousel.min.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/magnific-popup.css" type="text/css">
+<link rel="stylesheet" href="/assets/css/slicknav.min.css" type="text/css">
+<link rel="stylesheet" href="<?php echo '/assets/css/main.css' . $phpVersion; ?>">
+
+<!--
+ _   __           ___          _
+| | / /_ _____   / _ \___ ___ (_)__ ____
+| |/ / // / -_) / // / -_|_-</ / _ `/ _ \
+|___/\_,_/\__/ /____/\__/___/_/\_, /_//_/
+                              /___/
+-->
+
+</head>
+
 <body>
 
     <!-- Page Preloder -->
